e1d4dde32c769d85d2d61d77ee9360200d916d1e
suleymangunes <suleymangunesmail@gmail.com>
2024-05-20 09:53:19 +0300

    Gemini chat added

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,92 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gemini/flutter_gemini.dart';
+import 'package:gemini_chat_app/sections/chat.dart';
+import 'package:gemini_chat_app/sections/chat_stream.dart';
+import 'package:gemini_chat_app/sections/embed_batch_contents.dart';
+import 'package:gemini_chat_app/sections/embed_content.dart';
+import 'package:gemini_chat_app/sections/response_widget_stream.dart';
+import 'package:gemini_chat_app/sections/stream.dart';
+import 'package:gemini_chat_app/sections/text_and_image.dart';
+import 'package:gemini_chat_app/sections/text_only.dart';
 
-void main() {
+void main() async {
+  /// flutter run --dart-define=apiKey='Your Api Key'
+  Gemini.init(
+      apiKey: "AIzaSyCU-0Vy6WVnW-EHh2L8vTmbNUfpHiByroE", enableDebugging: true);
+
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Flutter Gemini',
+      themeMode: ThemeMode.dark,
+      debugShowCheckedModeBanner: false,
+      darkTheme: ThemeData.dark().copyWith(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+          cardTheme: CardTheme(color: Colors.blue.shade900)),
+      home: const MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
+class SectionItem {
+  final int index;
   final String title;
+  final Widget widget;
+
+  SectionItem(this.index, this.title, this.widget);
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  int _selectedItem = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  final _sections = <SectionItem>[
+    SectionItem(0, 'Stream text', const SectionTextStreamInput()),
+    SectionItem(1, 'textAndImage', const SectionTextAndImageInput()),
+    SectionItem(2, 'chat', const SectionChat()),
+    SectionItem(3, 'Stream chat', const SectionStreamChat()),
+    SectionItem(4, 'text', const SectionTextInput()),
+    SectionItem(5, 'embedContent', const SectionEmbedContent()),
+    SectionItem(6, 'batchEmbedContents', const SectionBatchEmbedContents()),
+    SectionItem(
+        7, 'response without setState()', const ResponseWidgetSection()),
+  ];
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text(_selectedItem == 0
+            ? 'Flutter Gemini'
+            : _sections[_selectedItem].title),
+        actions: [
+          PopupMenuButton<int>(
+            initialValue: _selectedItem,
+            onSelected: (value) => setState(() => _selectedItem = value),
+            itemBuilder: (context) => _sections.map((e) {
+              return PopupMenuItem<int>(value: e.index, child: Text(e.title));
+            }).toList(),
+            child: const Icon(Icons.more_vert_rounded),
+          )
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+      body: IndexedStack(
+        index: _selectedItem,
+        children: _sections.map((e) => e.widget).toList(),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
